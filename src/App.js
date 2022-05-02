473ae0cb4850cbd1a129bf9c8a3741d888914997
AlexAarmeniei <99760444+AlexAarmeniei@users.noreply.github.com>
2022-05-02 20:29:42 +0100

    Start NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./style/App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import { Contact } from "./components/Pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <div className="pages">
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/contact" component={Contact} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
